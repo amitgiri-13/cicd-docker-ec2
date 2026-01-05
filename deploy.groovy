6625618faa5a950b4b781c3e3b43be1969d76cc8
amit <amit101giri@gmail.com>
2026-01-05 10:09:01 +0545

    jenkinsfiles

--- deploy.groovy
+++ deploy.groovy
@@ -50,7 +50,7 @@ pipeline {
 
                       docker compose pull
                       docker compose up -d --build
-                    EOF
+                      EOF
                     '''
                 }
             }
