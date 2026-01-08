77578b5516c2ecf5a6155b183f2ef7aaf55e714b
amit <amit101giri@gmail.com>
2026-01-08 13:53:24 +0545

    cicd: jenkins

--- deploy.groovy
+++ deploy.groovy
@@ -22,12 +22,12 @@ pipeline {
                 string(credentialsId: "DOCKER_HUB_PASSWORD", variable: "DOCKER_HUB_PASSWORD")
             ]
             ){
-                sh """
+                sh '''
                     set -e 
                     echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USER --password-stdin
                     docker build -t "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG" .
                     docker push "$DOCKER_HUB_USER/$DOCKER_HUB_REPO:$TAG"
-                """
+                '''
             } 
             }       
         }
